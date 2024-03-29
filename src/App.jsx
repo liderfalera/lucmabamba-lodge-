de5d8be00caeac98a2a8982f2972e068ac7dd93c
liderfalera <lider.falera@usil.pe>
2024-03-28 19:04:10 -0500

    change: enabling complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Home } from "@pages/Home";
 
 function App() {
-	// return <Home />;
-	return <h1>Hola mundo</h1>;
+	return <Home />;
+	// return <h1>Hola mundo</h1>;
 }
 
 export default App;
